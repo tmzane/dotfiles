82d7e47b54b743e90e05faf2aaec92b252c61b2d
Tom <github@junk1tm.me>
2023-01-25 17:59:43 +0400

    add config.fish and fish_plugins

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+# set default editor to micro
+set --global --export EDITOR micro
+
+# disable homebrew analytics
+set --global --export HOMEBREW_NO_ANALYTICS 1
+# disable homebrew autoupdate
+set --global --export HOMEBREW_NO_AUTO_UPDATE 1
+
+# tell fzf.fish to include hidden files when searching
+set --global fzf_fd_opts --hidden --exclude=.git
+# rebind fzf.fish default key bindings
+fzf_configure_bindings --history=\e\b --variables=\e\cv
+
+if status is-interactive
+    # commands to run in interactive sessions
+end
