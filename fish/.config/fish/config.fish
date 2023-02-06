0e7b999fbe88876a6f41cf8d28f95a1318e5b71d
Tom <github@junk1tm.me>
2023-02-06 15:01:09 +0400

    fish: add HOMEBREW_NO_INSTALL_FROM_API variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,8 @@ set --global --export EDITOR micro
 set --global --export HOMEBREW_NO_ANALYTICS 1
 # disable homebrew autoupdate
 set --global --export HOMEBREW_NO_AUTO_UPDATE 1
+# disable homebrew install from API
+set --global --export HOMEBREW_NO_INSTALL_FROM_API 1
 
 # tell fzf.fish to include hidden files when searching
 set --global fzf_fd_opts --hidden --exclude=.git
