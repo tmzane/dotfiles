37307b0bf0c6d31d9edec1ee2cdc91b33958efa2
Tom <github@junk1tm.me>
2023-03-01 04:55:07 +0400

    fish: add aliases and fzf defaults

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,9 +8,14 @@ set --global --export HOMEBREW_NO_AUTO_UPDATE 1
 # disable homebrew install from API
 set --global --export HOMEBREW_NO_INSTALL_FROM_API 1
 
+# set fzf default command and options
+set --global --export FZF_DEFAULT_COMMAND "fd --hidden"
+set --global --export FZF_DEFAULT_OPTS "--reverse --border --multi --marker=+"
+
 # tell fzf.fish to include hidden files when searching
-set --global fzf_fd_opts --hidden --exclude=.git
+set --global fzf_fd_opts --hidden
 # rebind fzf.fish default key bindings
+# history: ctrl+opt+h; variables: ctrl+opt+v (use fish_key_reader)
 fzf_configure_bindings --history=\e\b --variables=\e\cv
 
 if status is-interactive
@@ -18,10 +23,14 @@ if status is-interactive
     abbr --add gs git status --short
     abbr --add gl git log --oneline
     abbr --add ga git add
-    abbr --add gc git commit --message
+    abbr --add gcm git commit --message
     abbr --add gw git switch
 
     # add misc abbreviations
     abbr --add p3 python3
     abbr --add \? tldr
+
+    # add command aliases
+    alias cat=bat
+    alias top=htop
 end
