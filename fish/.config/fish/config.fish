9fba601071f263829b5b390133d268f750f718e2
Tom <73077675+tmzane@users.noreply.github.com>
2024-01-27 23:20:24 +0500

    fish: update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -26,14 +26,14 @@ if status is-interactive
     set fish_cursor_insert line
 
     # add git abbreviations
-    abbr --add gw "git switch"
-    abbr --add gl "git log --oneline"
-    abbr --add gs "git status --short"
-    abbr --add gd "git diff"
-    abbr --add ga "git add"
-    abbr --add gc "git commit"
+    abbr --add gw  "git switch"
+    abbr --add gl  "git log --oneline"
+    abbr --add gs  "git status --short"
+    abbr --add gd  "git diff"
+    abbr --add ga  "git add"
+    abbr --add gc  "git commit"
     abbr --add gcm "git commit --message"
-    abbr --add gp "git push"
+    abbr --add gp  "git push"
     abbr --add gpf "git push --force-with-lease"
     abbr --add gpl "git pull --rebase"
     abbr --add gst "git stash"
@@ -42,13 +42,15 @@ if status is-interactive
     abbr --add \? "tldr"
     abbr --add \?\? "man"
     abbr --add py "python3"
-    abbr --add vpnon "sudo wg-quick up wg0"
-    abbr --add vpnoff "sudo wg-quick down wg0"
 
     # replace builtin tools
     alias cat bat
     alias top htop
     alias vim nvim
+
+    # add vpn aliases
+    alias vpnon "sudo wg-quick up wg0"
+    alias vpnoff "sudo wg-quick down wg0"
 
     # add "Make a dir and cd into it" command
     function md; set dir $argv[1]; mkdir -p $dir && cd $dir; end
