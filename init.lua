2252ef4b58a666429d286a0360a9bdf052191ae5
Tom <73077675+tmzane@users.noreply.github.com>
2024-04-14 23:20:28 +0500

    nvim: disable optional providers

--- init.lua
+++ init.lua
@@ -64,6 +64,12 @@ local function setup_editor_options()
         pattern = { "c" },
         callback = function() vim.bo.commentstring = "// %s" end,
     })
+
+    -- disable optional providers
+    vim.g.loaded_python3_provider = 0
+    vim.g.loaded_ruby_provider = 0
+    vim.g.loaded_node_provider = 0
+    vim.g.loaded_perl_provider = 0
 end
 
 local function setup_keymaps()
