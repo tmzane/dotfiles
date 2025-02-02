40d2693e98dddf5a65de900e39a58df17ff6777c
Nik <73077675+tmzane@users.noreply.github.com>
2025-02-03 01:02:19 +0200

    fish: update config

--- config.fish
+++ config.fish
@@ -1,29 +1,26 @@
-set --export EDITOR "nvim"
+set --export EDITOR   "nvim"
 set --export MANPAGER "nvim +Man!"
+
+set --export HOMEBREW_NO_ANALYTICS   1
+set --export HOMEBREW_NO_AUTO_UPDATE 1
+
+set --export FZF_DEFAULT_OPTS "--reverse --border --preview-window=70%"
 
 fish_add_path --global --path --move /opt/homebrew/bin
 fish_add_path --global --path --move ~/go/bin
 
-# brew: disable analytics
-set --export HOMEBREW_NO_ANALYTICS 1
-# brew: disable autoupdate
-set --export HOMEBREW_NO_AUTO_UPDATE 1
-
-# fzf: set default options
-set --export FZF_DEFAULT_OPTS "--reverse --border --preview-window=70%"
-
 if status is-interactive
-    # fzf.fish: include hidden files when searching
+    fish_vi_key_bindings
+    set fish_cursor_default  block blink
+    set fish_cursor_insert   line  blink
+    set fish_cursor_external line  blink
+
     set fzf_fd_opts --hidden
-    # fzf.fish: set custom key bindings
     fzf_configure_bindings --directory=\ef --git_log=\el --git_status=\es --history=\eh --processes=\ep --variables=\ev
 
-    # enable vim mode
-    fish_vi_key_bindings
-    set fish_cursor_default block blink
-    set fish_cursor_insert  line  blink
+    bind --mode default \cz "fg 2>/dev/null; commandline -f repaint-mode"
+    bind --mode insert  \cz "fg 2>/dev/null; commandline -f repaint-mode"
 
-    # add git abbreviations
     abbr --add gw  "git switch"
     abbr --add gl  "git log --oneline"
     abbr --add gs  "git status --short"
@@ -36,20 +33,13 @@ if status is-interactive
     abbr --add gpf "git push --force-with-lease"
     abbr --add gpl "git pull --rebase"
     abbr --add gst "git stash"
+    abbr --add py  "python3"
 
-    # add misc abbreviations
-    abbr --add \?   "tldr"
-    abbr --add \?\? "man"
-    abbr --add py   "python3"
-
-    # replace builtin tools
-    alias cat bat
-    alias top htop
-    alias vim nvim
-
-    # add vpn aliases
-    alias vpnon  "sudo wg-quick up wg0"
-    alias vpnoff "sudo wg-quick down wg0"
+    alias cat     bat
+    alias top     htop
+    alias vim     nvim
+    alias vpnup   "sudo wg-quick up wg0"
+    alias vpndown "sudo wg-quick down wg0"
 
     # md: make a directory and cd into it
     function md; set dir $argv[1]; mkdir -p $dir && cd $dir; end
