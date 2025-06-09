62dd054cbd7285015dd6b0b1248654c329c206db
Nik <73077675+tmzane@users.noreply.github.com>
2025-06-09 10:36:37 +0300

    fish: update config

--- config.fish
+++ config.fish
@@ -1,49 +1,62 @@
+# setup environment
 set --export EDITOR                  nvim
 set --export MANPAGER                nvim +Man!
+set --export BAT_THEME               base16
+# set --export BAT_THEME_LIGHT         OneHalfLight
+# set --export BAT_THEME_DARK          OneHalfDark
 set --export BROWSER                 librewolf
 set --export CONTAINER_RUNNER        podman
 set --export HOMEBREW_NO_ANALYTICS   1
 set --export HOMEBREW_NO_AUTO_UPDATE 1
+set --export FZF_DEFAULT_OPTS        "--tmux=100% --border=none --info=hidden"
+set --export FZF_CTRL_T_OPTS         "--no-reverse --preview='bat --number --color=always {}' --preview-window=border-left"
 
-set --export FZF_DEFAULT_OPTS "--reverse --border --preview-window=70%"
-
-fish_add_path --global --path --move /opt/homebrew/bin
+# setup $PATH
 fish_add_path --global --path --move ~/go/bin
+fish_add_path --global --path --move /opt/homebrew/bin
 
-if status is-interactive
-    fish_vi_key_bindings
-    set fish_cursor_default  block blink
-    set fish_cursor_insert   line  blink
-    set fish_cursor_external line  blink
+# setup vi mode
+fish_vi_key_bindings
+set fish_cursor_default block blink
+set fish_cursor_insert  line  blink
 
-    set fzf_fd_opts --hidden
-    fzf_configure_bindings --directory=\ef --git_log=\el --git_status=\es --history=\eh --processes=\ep --variables=\ev
+# setup keymaps
+bind --mode default \cz "fg 2>/dev/null; commandline -f repaint-mode"
+bind --mode insert  \cz "fg 2>/dev/null; commandline -f repaint-mode"
 
-    bind --mode default \cz "fg 2>/dev/null; commandline -f repaint-mode"
-    bind --mode insert  \cz "fg 2>/dev/null; commandline -f repaint-mode"
+# setup prompt
+# https://github.com/pure-fish/pure
+set pure_show_jobs true
 
-    abbr --add gw  "git switch"
-    abbr --add gl  "git log --oneline"
-    abbr --add gs  "git status --short"
-    abbr --add gd  "git diff"
-    abbr --add ga  "git add"
-    abbr --add gap "git add --patch"
-    abbr --add gc  "git commit"
-    abbr --add gcm "git commit --message"
-    abbr --add gp  "git push"
-    abbr --add gpf "git push --force-with-lease"
-    abbr --add gf  "git fetch --all"
-    abbr --add gpl "git pull --rebase"
-    abbr --add py  "python3"
+# setup fzf
+# https://github.com/junegunn/fzf
+fzf --fish | source
+# https://github.com/PatrickF1/fzf.fish
+set fzf_fd_opts --hidden
+fzf_configure_bindings --directory=\ef --git_log=\el --git_status=\es --history=\eh --processes=\ep --variables=\ev
 
-    alias cat     bat
-    alias top     htop
-    alias vim     nvim
-    alias vpnup   "sudo wg-quick up wg0"
-    alias vpndown "sudo wg-quick down wg0"
+# setup aliases
+alias cat     bat
+alias top     htop
+alias vim     nvim
+alias vpnup   "sudo wg-quick up wg0"
+alias vpndown "sudo wg-quick down wg0"
 
-    # md: make a directory and cd into it
-    function md; set dir $argv[1]; mkdir -p $dir && cd $dir; end
-    # ql: preview with Quick Look
-    function ql; qlmanage -p $argv &> /dev/null; end
-end
+# setup abbreviations
+abbr --add ga  "git add"
+abbr --add gap "git add --patch"
+abbr --add gc  "git commit"
+abbr --add gca "git commit --amend --no-edit"
+abbr --add gcm "git commit --message"
+abbr --add gd  "git diff HEAD"
+abbr --add gf  "git fetch --all"
+abbr --add gl  "git log --oneline"
+abbr --add gp  "git push"
+abbr --add gpf "git push --force-with-lease"
+abbr --add gpl "git pull --rebase"
+abbr --add gr  "git rebase"
+abbr --add gri "git rebase --interactive"
+abbr --add gs  "git status --short"
+abbr --add gst "git stash"
+abbr --add gw  "git switch"
+abbr --add py  python3
