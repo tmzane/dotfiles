53821d80b77038a510616ae775ddd0dc8b40cb96
Nik <73077675+tmzane@users.noreply.github.com>
2025-07-13 21:49:40 +0300

    fish: cleanups

--- config.fish
+++ config.fish
@@ -1,13 +1,10 @@
 # setup environment
-set --export EDITOR                  nvim
-set --export MANPAGER                nvim +Man!
-set --export BAT_THEME               base16
-# set --export BAT_THEME_LIGHT         OneHalfLight
-# set --export BAT_THEME_DARK          OneHalfDark
-set --export BROWSER                 librewolf
-set --export CONTAINER_RUNNER        podman
-set --export HOMEBREW_NO_ANALYTICS   1
-set --export HOMEBREW_NO_AUTO_UPDATE 1
+set --export EDITOR                  "nvim"
+set --export MANPAGER                "nvim +Man!"
+set --export BAT_THEME               "base16"
+set --export CONTAINER_RUNNER        "podman"
+set --export HOMEBREW_NO_ANALYTICS   "1"
+set --export HOMEBREW_NO_AUTO_UPDATE "1"
 set --export FZF_DEFAULT_OPTS        "--tmux=100% --border=none --info=hidden"
 set --export FZF_CTRL_T_OPTS         "--no-reverse --preview='bat --number --color=always {}' --preview-window=border-left"
 
@@ -17,12 +14,13 @@ fish_add_path --global --path --move ~/go/bin
 
 # setup vi mode
 fish_vi_key_bindings
-set fish_cursor_default block blink
-set fish_cursor_insert  line  blink
+set fish_cursor_default  block blink
+set fish_cursor_insert   line  blink
+set fish_cursor_external line  blink # for REPLs
 
 # setup keymaps
-bind --mode default \cz "fg 2>/dev/null; commandline -f repaint-mode"
-bind --mode insert  \cz "fg 2>/dev/null; commandline -f repaint-mode"
+bind --mode default \cz "fg 2>/dev/null; commandline --function repaint-mode"
+bind --mode insert  \cz "fg 2>/dev/null; commandline --function repaint-mode"
 
 # setup prompt
 starship init fish | source
@@ -35,9 +33,9 @@ set fzf_fd_opts --hidden
 fzf_configure_bindings --directory=\ef --git_log=\el --git_status=\es --history=\eh --processes=\ep --variables=\ev
 
 # setup aliases
-alias cat     bat
-alias top     htop
-alias vim     nvim
+alias cat     "bat"
+alias top     "htop"
+alias vim     "nvim"
 alias vpnup   "sudo wg-quick up wg0"
 alias vpndown "sudo wg-quick down wg0"
 
@@ -58,4 +56,4 @@ abbr --add gri "git rebase --interactive"
 abbr --add gs  "git status --short"
 abbr --add gst "git stash"
 abbr --add gw  "git switch"
-abbr --add py  python3
+abbr --add py  "python3"
