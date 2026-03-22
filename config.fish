6e6bba02bd69f0f7c10c26e8dbc6c3a19a461229
Nick <73077675+tmzane@users.noreply.github.com>
2026-03-22 23:14:50 +0200

    fish: update config, remove plugins

--- config.fish
+++ config.fish
@@ -1,60 +1,105 @@
-# setup environment
-set --export EDITOR                  "nvim"
-set --export MANPAGER                "nvim +Man!"
-set --export BAT_THEME               "base16"
-set --export CONTAINER_RUNNER        "podman"
-set --export HOMEBREW_NO_ANALYTICS   "1"
-set --export HOMEBREW_NO_AUTO_UPDATE "1"
-set --export FZF_DEFAULT_OPTS        "--tmux=100% --border=none --info=hidden"
-set --export FZF_CTRL_T_OPTS         "--no-reverse --preview='bat --number --color=always {}' --preview-window=border-left"
+# Setup environment.
+set --export EDITOR                  'nvim'
+set --export MANPAGER                'nvim +Man!'
+set --export BAT_THEME               'base16'
+set --export CONTAINER_RUNNER        'podman'
+set --export HOMEBREW_NO_ANALYTICS   '1'
+set --export HOMEBREW_NO_AUTO_UPDATE '1'
+set --export FZF_DEFAULT_OPTS        '--border=none --inline-info'
 
-# setup $PATH
+# Setup $PATH.
 fish_add_path --global --path --move /opt/homebrew/bin
 fish_add_path --global --path --move ~/go/bin
 
-# setup vi mode
+# Setup prompt.
+set fish_greeting ''
+starship init fish | source
+
+# Setup VI mode.
 fish_vi_key_bindings
 set fish_cursor_default  block blink
 set fish_cursor_insert   line  blink
-set fish_cursor_external line  blink # for REPLs
+set fish_cursor_external line  blink # For REPLs.
 
-# setup keymaps
-bind --mode default \cz "fg 2>/dev/null; commandline --function repaint-mode"
-bind --mode insert  \cz "fg 2>/dev/null; commandline --function repaint-mode"
+# Setup keymaps.
+# https://fishshell.com/docs/current/interactive.html#shared-bindings
+# https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+bind --mode default ctrl-z _fuzzy_jobs repaint
+bind --mode insert  ctrl-z _fuzzy_jobs repaint
+bind --mode insert  ctrl-f _fuzzy_find
+bind --mode insert  ctrl-r _fuzzy_history
 
-# setup prompt
-set fish_greeting ""
-starship init fish | source
+# Setup aliases.
+alias cat 'bat'
+alias top 'htop'
+alias vim 'nvim'
+alias fd  'LS_COLORS="" command fd' # https://github.com/sharkdp/fd/issues/1031
 
-# setup fzf
-# https://github.com/junegunn/fzf
-fzf --fish | source
-# https://github.com/PatrickF1/fzf.fish
-set fzf_fd_opts --hidden
-fzf_configure_bindings --directory=\ef --git_log=\el --git_status=\es --history=\eh --processes=\ep --variables=\ev
+# Setup abbreviations.
+abbr --add ga  'git add'
+abbr --add gap 'git add --patch'
+abbr --add gc  'git commit'
+abbr --add gcm 'git commit --message'
+abbr --add gd  'git diff'
+abbr --add gdh 'git diff HEAD'
+abbr --add gds 'git diff --staged'
+abbr --add gf  'git fetch'
+abbr --add gl  'git log -10 --oneline'
+abbr --add gp  'git push'
+abbr --add gpf 'git push --force-with-lease'
+abbr --add gpl 'git pull'
+abbr --add gr  'git rebase'
+abbr --add gri 'git rebase --interactive'
+abbr --add gs  'git status --short'
+abbr --add gst 'git stash'
+abbr --add gsw 'git switch'
+abbr --add gwt 'git worktree'
+abbr --add py  'python3'
 
-# setup aliases
-alias cat "bat"
-alias top "htop"
-alias vim "nvim"
+function vimgrep
+    rg --vimgrep $argv | nvim -q - +copen
+end
 
-# setup abbreviations
-abbr --add ga  "git add"
-abbr --add gap "git add --patch"
-abbr --add gc  "git commit"
-abbr --add gca "git commit --amend --no-edit"
-abbr --add gcm "git commit --message"
-abbr --add gd  "git diff"
-abbr --add gdh "git diff HEAD"
-abbr --add gds "git diff --staged"
-abbr --add gf  "git fetch --all"
-abbr --add gl  "git log --oneline"
-abbr --add gp  "git push"
-abbr --add gpf "git push --force-with-lease"
-abbr --add gpl "git pull --rebase --autostash"
-abbr --add gr  "git rebase"
-abbr --add gri "git rebase --interactive"
-abbr --add gs  "git status --short"
-abbr --add gst "git stash"
-abbr --add gw  "git switch"
-abbr --add py  "python3"
+function vimfind
+    nvim $(fd --type=file --full-path --hidden --no-require-git $argv)
+end
+
+function _fuzzy_jobs
+    if not builtin jobs --query
+        return
+    end
+
+    set --local fzf_opts \
+        --select-1 \
+        --nth=4 \
+        --accept-nth=2 \
+        --prompt='Jobs: '
+
+    jobs | fzf $fzf_opts | read --local pid; and builtin fg $pid 2>/dev/null
+end
+
+function _fuzzy_find
+    set --local fd_opts \
+        --hidden \
+        --color=always \
+        --no-require-git
+
+    set --local fzf_opts \
+        --ansi \
+        --multi \
+        --preview='bat --color=always {}' \
+        --preview-window='right,60%,border-left' \
+        --prompt='Files: '
+
+    fd $fd_opts | fzf $fzf_opts | string join ' ' | read --local out; and commandline --insert $out
+end
+
+function _fuzzy_history
+    set --local fzf_opts \
+        --read0 \
+        --preview='echo {}' \
+        --preview-window='down,20%,border-up' \
+        --prompt='History: '
+
+    history --null | fzf $fzf_opts | read --null --local out; and commandline --replace $out
+end
